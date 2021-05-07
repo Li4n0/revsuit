b40548bcac5841fa18c438162bffbd74ed1d01f9
Li4n0 <li4n06@gmail.com>
2021-05-07 22:58:51 +0800

    feat(cli): support generate default config

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -7,11 +7,15 @@ import (
 
 	"github.com/li4n0/revsuit/pkg/server"
 	"github.com/urfave/cli/v2"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 	log "unknwon.dev/clog/v2"
 )
 
 func Start() {
+	_ = log.NewConsole(100,
+		log.ConsoleConfig{
+			Level: log.LevelInfo,
+		})
 	app := &cli.App{
 		Name:  "RevSuit",
 		Usage: "An Open-Sourced Reverse Platform Designed for Receive Various Kinds of Connection",
@@ -25,20 +29,35 @@ func Start() {
 				Usage: "token used to manage platform",
 			},
 			&cli.StringFlag{
-				Name:  "flags",
-				Usage: "for http and dns connection, platform will only record the connection those match these regex flags. * meaning record all",
-			},
-			&cli.StringFlag{
 				Name:  "db",
 				Usage: "database file path",
 			},
+			&cli.StringFlag{
+				Name:  "log",
+				Usage: "specify a log level from debug, info, warn, error, fatal. (default: info)",
+			},
+			&cli.PathFlag{
+				Name:  "config",
+				Usage: "load configuration from FILE (default: config.yaml)",
+			},
 		},
 		Action: func(c *cli.Context) error {
+			var configFile = "config.yaml"
+			if c.Path("config") != "" {
+				configFile = c.Path("config")
+			}
 			conf := &server.Config{}
-			if content, err := os.ReadFile("config.yaml"); err == nil {
+			if content, err := os.ReadFile(configFile); err == nil {
 				if err := yaml.Unmarshal(content, conf); err != nil {
 					return err
 				}
+			} else if os.IsNotExist(err) && configFile == "config.yaml" {
+				log.Warn("Generate default configurations to config.yaml, please configure and run again.")
+				err := os.WriteFile("config.yaml", configTemplate, 0644)
+				if err != nil {
+					return err
+				}
+				return nil
 			} else {
 				return err
 			}
@@ -50,6 +69,9 @@ func Start() {
 			}
 			if c.String("db") != "" {
 				conf.Database = c.String("db")
+			}
+			if c.String("log") != "" {
+				conf.Database = c.String("log")
 			}
 			server.New(conf).Run()
 			return nil

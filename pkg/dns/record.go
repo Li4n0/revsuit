4ad6a4df0304d6bedaf254d1f5f1724e9c4f9bc7
Li4n0 <li4n06@gmail.com>
2022-09-13 23:42:34 +0800

    fix: fix the error of `database is locked` when use sqlite
    use db-level lock to prevent too much write operation lead to error of `database is locked` when use
    sqlite. #54
    
    fix #54

--- pkg/dns/record.go
+++ pkg/dns/record.go
@@ -41,6 +41,13 @@ func newRecord(rule *Rule, flag, domain, remoteIp, ipArea string) (r *Record, er
 		Domain: domain,
 		Rule:   *rule,
 	}
+
+	// sqlite db-level lock to prevent too much write operation lead to error of `database is locked` #54
+	if database.Driver == database.Sqlite {
+		database.Locker.Lock()
+		defer database.Locker.Unlock()
+	}
+
 	return r, database.DB.Create(r).Error
 }
 
